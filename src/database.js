1ec3fe6bd4455affca00c35f687b08da90ce78ec
Guillaume97490 <techer.guillaume97490@gmail.com>
2020-03-05 15:37:14 +0400

    add config heroku BDD

--- src/database.js
+++ src/database.js
@@ -1,10 +1,16 @@
 const DATABASE = require('./env.database');
 const Sequelize = require('sequelize');
+if (process.env.CLEARDB_DATABASE_URL){
+    DATABASE.LOCAL.DBNAME = process.env.DB;
+    DATABASE.LOCAL.USERNAME = process.env.USER;
+    DATABASE.LOCAL.PASSWORD = process.env.PASSWORD;
+}
 const sequelize = new Sequelize(DATABASE.LOCAL.DBNAME, DATABASE.LOCAL.USERNAME, DATABASE.LOCAL.PASSWORD, { // nom de la BDD, username, password
-host: 'localhost',
+host: process.env.HOST||'localhost',
 dialect: 'mysql',
 logging: false,//passer a true pour voir les différentes requêtes effectuées par l'ORM
 });
+
 //on exporte pour utiliser notre connexion depuis les autre fichiers.
 var exports = module.exports = {};
 exports.sequelize = sequelize;
