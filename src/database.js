fac079cf2c7732a17f11e3a9967804ba89b1cd31
Guillaume97490 <techer.guillaume97490@gmail.com>
2020-03-05 18:42:55 +0400

    fix config

--- src/database.js
+++ src/database.js
@@ -1,17 +1,29 @@
-const DATABASE = require('./env.database');
+let DATABASE = require('./env.database');
 const Sequelize = require('sequelize');
+
+
 if (process.env.CLEARDB_DATABASE_URL){
-    const DATABASE = {};
+    DATABASE = {};
     DATABASE.LOCAL.DBNAME = process.env.DB;
     DATABASE.LOCAL.USERNAME = process.env.USER;
     DATABASE.LOCAL.PASSWORD = process.env.PASSWORD;
-}
-const sequelize = new Sequelize(DATABASE.LOCAL.DBNAME, DATABASE.LOCAL.USERNAME, DATABASE.LOCAL.PASSWORD, { // nom de la BDD, username, password
-host: process.env.HOST||'localhost',
-dialect: 'mysql',
-logging: false,//passer a true pour voir les différentes requêtes effectuées par l'ORM
-});
 
-//on exporte pour utiliser notre connexion depuis les autre fichiers.
-var exports = module.exports = {};
-exports.sequelize = sequelize;
+    const sequelize = new Sequelize(DATABASE.LOCAL.DBNAME, DATABASE.LOCAL.USERNAME, DATABASE.LOCAL.PASSWORD, { // nom de la BDD, username, password
+        host: process.env.HOST||'localhost',
+        dialect: 'mysql',
+        logging: false,//passer a true pour voir les différentes requêtes effectuées par l'ORM
+    });
+    
+    return module.exports = sequelize;
+}
+if (!process.env.CLEARDB_DATABASE_URL){
+    const sequelize = new Sequelize(DATABASE.LOCAL.DBNAME, DATABASE.LOCAL.USERNAME, DATABASE.LOCAL.PASSWORD, { // nom de la BDD, username, password
+    host: process.env.HOST||'localhost',
+    dialect: 'mysql',
+    logging: false,//passer a true pour voir les différentes requêtes effectuées par l'ORM
+    });
+    
+    //on exporte pour utiliser notre connexion depuis les autre fichiers.
+    var exports = module.exports = {};
+    exports.sequelize = sequelize;
+}
